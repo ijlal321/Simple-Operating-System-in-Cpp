367120c5c4e14768746536fad058568d29dbaa8b
ijlal123 <ijlal.tanveer291@gmail.com>
2025-12-09 13:27:31 +0500

    added structure for project 2

--- 1_HelloWorld/src/render.cpp
+++ 1_HelloWorld/src/render.cpp
@@ -9,16 +9,20 @@
 void WindowClass::Draw(std::string_view label)
 {
     constexpr static auto window_flags =
-        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+    ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
     constexpr static auto window_size = ImVec2(1280.0F, 720.0F);
     constexpr static auto window_pos = ImVec2(0.0F, 0.0F);
-
+    
     ImGui::SetNextWindowSize(window_size);
     ImGui::SetNextWindowPos(window_pos);
-
+    
+    
     ImGui::Begin(label.data(), nullptr, window_flags);
+    
 
+    ImGui::SetCursorPos(ImVec2(200,200));
+    ImGui::Text("heello man");
     ImGui::End();
 }
 
